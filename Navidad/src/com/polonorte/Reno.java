41a243af91bd2b69d00fb50bcecddb621e5d9e08
diegoProFP <diego.di1@educa.madrid.org>
2022-01-31 17:16:28 +0100

    Subida proyecto inicial

---
+++ Navidad/src/com/polonorte/Reno.java
@@ -0,0 +1,36 @@
+package com.polonorte;
+
+/**
+ * Clase que representa un reno
+ * @author Diego
+ *
+ * @version 1.0
+ */
+public class Reno {
+
+    private String nombre;
+    private int velocidad;
+
+    private int numCuernos;
+    /**
+     * Metodo para que un reno se ponga a volar
+     * @param direccion La direccion a la que tiene que ir el reno
+     * @return El grito de guerra que devuelve el reno
+     */
+    public String volar(String direccion){
+        System.out.println("Reno.volar");
+
+        return "juasss";
+    }
+
+    /**
+     * Metodo que hace correr a este reno hacia una direccion
+     * @deprecated Deprecado desde la version 0.5, mejor usar {@link #volar(String)}
+     * @param direccion Direccion a la que tiene que ir el reno
+     */
+    public void correr(String direccion){
+        //TODO terminar
+    }
+
+
+}
