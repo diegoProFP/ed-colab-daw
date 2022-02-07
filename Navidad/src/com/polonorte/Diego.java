53f37de00d869b26496d75a5384a18429b5c8056
diegoProFP <diego.di1@educa.madrid.org>
2022-02-07 19:58:39 +0100

    FFFDFD

--- Navidad/src/com/polonorte/Diego.java
+++ Navidad/src/com/polonorte/Diego.java
@@ -1,7 +1,7 @@
 package com.polonorte;
 
 /**
- * <h1>Clase creada para 1DAW WEBAPP FACTORY paquito paco</h1>
+ * <h1>Clase creada para FUERA</h1>
  * <i>Todos los derechos reservados<i>. 
  * @author Diego
  *
