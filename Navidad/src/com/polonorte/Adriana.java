8e2debd38170d8d18227c7d125cffde3c07975f1
macarni <aperezconesa@gmail.com>
2022-02-07 20:35:59 +0100

    Se retoca el comentario

--- Navidad/src/com/polonorte/Adriana.java
+++ Navidad/src/com/polonorte/Adriana.java
@@ -1,7 +1,7 @@
 package com.polonorte;
 
 /**
- * <h1>Clase creada para 1DAW WEB APPLICATION COMPANY</h1>
+ * <h1>Clase creada para 1D.A.W. WEB APPLICATION COMPANY (y hecha con un poco de sueño)</h1>
  * <i>Todos los derechos reservados<i>. 
  * @author Adriana
  */
