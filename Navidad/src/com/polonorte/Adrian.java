0017fa553cac726a320e49af0480f0113b32a3db
AdriiiDAW <adriancis4@gmail.com>
2022-02-07 20:35:59 +0100

    Se retoca

--- Navidad/src/com/polonorte/Adrian.java
+++ Navidad/src/com/polonorte/Adrian.java
@@ -1,7 +1,7 @@
 package com.polonorte;
 
 /**
- * <h1>Clase creada para 1DAW WEB APPLICATION COMPANY</h1>
+ * <h1>Clase creada para 1D.A.W WEB APPLICATION COMPANY(y hecha con mucho cariño)</h1>
  * <i>Todos los derechos reservados<i>. 
  * @author 1dawa
  *
