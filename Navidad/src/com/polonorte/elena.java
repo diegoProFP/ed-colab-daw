a8a4b1f742b77f0ff4cb3aee227afb49bd5b4bd3
ElenaDelLla <elena.delgado.llamas@gmail.com>
2022-02-07 20:36:20 +0100

    se retoca el comentario

--- Navidad/src/com/polonorte/elena.java
+++ Navidad/src/com/polonorte/elena.java
@@ -1,11 +1,13 @@
 package com.polonorte;
 
 /**
- * <h1>Clase creada para 1DAW WEB APPLICATION COMPANY</h1>
+ * <h1>Clase creada para 1D.A.W. WEB APPLICATION COMPANY (y hecha con mucho cariño)</h1>
  * <i>Todos los derechos reservados<i>. 
  * @author elena
  *
  */
 public class elena {
+	
+	
 
 }
